ab52dd3ed30061fc8d1696ad4e2447d9ae3b3e77
cameronstewart <cameron@projectronin.com>
2021-10-13 15:18:15 -0600

    comment out internet check

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -30,13 +30,14 @@ while [ "$#" -gt 0 ]; do
     shift
 done
 
-echo "Checking internet connection"
-curl -sSf tx.fhir.org > /dev/null
-
-if [ $? -ne 0 ] ; then
-  echo "Offline (or the terminology server is down), unable to update.  Exiting"
-  exit 1
-fi
+# Commenting out - ping isn't allowed to use in Github Action
+#echo "Checking internet connection"
+#curl -sSf tx.fhir.org > /dev/null
+#
+#if [ $? -ne 0 ] ; then
+#  echo "Offline (or the terminology server is down), unable to update.  Exiting"
+#  exit 1
+#fi
 
 if [ ! -d "$input_cache_path" ] ; then
   if [ $FORCE != true ]; then
