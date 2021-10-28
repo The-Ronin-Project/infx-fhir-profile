109ce786056ee7d3085298715f0b0ef5912a5899
rmhayek <raja@projectronin.com>
2021-10-27 20:35:19 -0700

    added -v option to include detected ed visits.  added note encoding in doc ref. staging directory and run sushi every time

--- _genSamples.sh
+++ _genSamples.sh
@@ -10,7 +10,10 @@ NC='\033[0m' # No Color
 file_count=1
 
 usage () {
-  echo -n "Usage: $0 -d {example|test}"
+  echo -n "Usage: $0 -d {example|test} -v"
+  echo ""
+  echo -n "use \"-v\" to include DetectedEdVisit tests."
+  echo ""
   exit 1
 }
 
@@ -18,9 +21,10 @@ if [ $# -eq 0 ]; then
   usage
 fi
 
-while getopts d: flag
+while getopts vd: flag
 do
   case "${flag}" in
+    v) with_edvisits=true;;
     d) data=${OPTARG};;
     *) usage ${OPTARG};;
   esac
@@ -35,12 +39,47 @@ else
   usage
 fi
 
+if ! type sushi > /dev/null; then
+  echo -e "${RED}ERROR: sushi is not installed.  Please install sushi.${NC}"
+  exit 1
+fi
+
 if ! type jq > /dev/null; then
   echo -e "${RED}ERROR: jq is not installed.  Please install jq.${NC}"
   exit 1
 fi
 
-files=`ls ${PWD}/fsh-generated/resources/*${fname_end}.json 2>/dev/null`
+sushi ${PWD}
+if [ ! $? ]
+then
+  echo -e "${RED}ERROR: sushi failed!${NC}"
+  exit 1
+fi
+
+TEMP_DIR="${PWD}/temp/stage"
+mkdir -p ${TEMP_DIR}
+cp ${PWD}/fsh-generated/resources/*${fname_end}.json ${TEMP_DIR}
+
+doc_ref=`ls ${TEMP_DIR}/*DocumentReference*${fname_end}.json 2>/dev/null`
+if [ ! -z "${doc_ref}" ]
+then
+  # Process DocumentReference content.attachment.data
+  for i in ${doc_ref}
+  do
+    dec_name=`cat ${i} | jq -r '.content[0].attachment.data' | base64 -d`
+    echo -e "${GREEN}Inserting ${dec_name} in ${i}.${NC}"
+    enc_file=`cat ${PWD}/custom/notes/${dec_name} | base64`
+    f=`jq '.content[0].attachment.data = $newVal' --arg newVal ${enc_file} <<< cat ${i}`
+    echo ${f} > ${i}
+  done
+fi
+
+if [ ${with_edvisits} ]
+then
+  cp ${PWD}/custom/resources/*${fname_end}.json ${TEMP_DIR} 2>/dev/null
+fi
+
+files=`ls ${TEMP_DIR}/*${fname_end}.json 2>/dev/null`
 
 if [ -z "${files}" ]
 then
@@ -67,4 +106,4 @@ cat ${data}.json | jq -c '.[]' > ${data}.ndjson
 gzip -9 ${data}.ndjson
 rm ${data}.json
 echo -e "${GREEN}Created ${PWD}/${data}.ndjson.gz that can be uploaded to devbox${NC}"
-
+rm -rf ${TEMP_DIR}
